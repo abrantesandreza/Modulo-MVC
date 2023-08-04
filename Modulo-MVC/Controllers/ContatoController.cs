f027306d7dd4d49d5f886321553f7ff3ce9a873b
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-08-04 19:45:11 -0300

    Injeção de Dependência e página de listagem de contato criada

--- Modulo-MVC/Controllers/ContatoController.cs
+++ Modulo-MVC/Controllers/ContatoController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Modulo_MVC.Repositories;
 
 namespace Modulo_MVC.Controllers
 {
     public class ContatoController : Controller
     {
+        private readonly AgendaRepository _dbContext;
+
+        public ContatoController(AgendaRepository dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            var contatos = _dbContext.Contatos.ToList();
+            return View(contatos);
         }
     }
 }
