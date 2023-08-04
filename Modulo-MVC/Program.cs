d2a8e1f8d958e0a74de85a751b1eadffc796bcf0
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-08-04 14:22:18 -0300

    EntityFramework configurado

--- Modulo-MVC/Program.cs
+++ Modulo-MVC/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Modulo_MVC.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaRepository>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
