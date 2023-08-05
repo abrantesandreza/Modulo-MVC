05ba7c59862b2d058bdeb46f26bbf6bd31ff4e95
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-08-04 22:38:23 -0300

    Método Criar como POST implementado

--- Modulo-MVC/Controllers/ContatoController.cs
+++ Modulo-MVC/Controllers/ContatoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Modulo_MVC.Models;
 using Modulo_MVC.Repositories;
 
 namespace Modulo_MVC.Controllers
@@ -24,6 +25,20 @@ namespace Modulo_MVC.Controllers
         public IActionResult Criar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Contatos.Add(contato);
+                _dbContext.SaveChanges();
+                
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(contato);
         }
     }
 }
